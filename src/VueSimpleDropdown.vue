0f48e6a3d88de8daa3df6b18092444c7f64458d7
kouts <koutsaftakis@gmail.com>
2022-08-14 18:44:39 +0300

    fix: fixed ts error

--- src/VueSimpleDropdown.vue
+++ src/VueSimpleDropdown.vue
@@ -2,4 +2,10 @@
   <div>VueSimpleDropdown</div>
 </template>
 
-<script lang="ts"></script>
+<script lang="ts">
+import { defineComponent } from 'Vue'
+
+export default defineComponent({
+  name: 'VueSimpleDropdown'
+})
+</script>
