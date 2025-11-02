ba17ed2fa3280945157438ad1b6b373e20dd381c
kouts <koutsaftakis@gmail.com>
2025-11-02 17:57:36 +0200

    test: converted jest config to ees modules

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   env: {
     test: {
       presets: [
@@ -6,11 +6,11 @@ module.exports = {
           '@babel/preset-env',
           {
             targets: {
-              node: 'current'
-            }
-          }
-        ]
-      ]
-    }
-  }
+              node: 'current',
+            },
+          },
+        ],
+      ],
+    },
+  },
 }
